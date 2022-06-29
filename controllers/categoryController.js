4fd796f8f162e5491e32416e37ff17a208319f8e
ahmed <ahmedachour789@gmail.com>
2022-06-29 13:01:03 +0100

    create controllers and test them

---
+++ controllers/categoryController.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/', async (req, res) => {
+
+      res.status(200).send("hellooo")
+
+  })
+
+  module.exports = app
--- index.js
+++ index.js
@@ -10,20 +10,36 @@ const app = express();
 const port = 3000;
 
 // autorisé les données de type JSON
-app.use(express.json())
+app.use(express.json());
 
 // autorisé les données de type files
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // autorisé l'accee d'un serveur
-app.use(cors())
+app.use(cors());
 
 // import controllers
-const storeController = require("./controllers/storeController")
+const storeController = require("./controllers/storeController");
+const commandeController = require("./controllers/commandeController");
+const productController = require("./controllers/productController");
+const sectionController = require("./controllers/sectionController");
+const userController = require("./controllers/userController");
+const categoryController = require("./controllers/categoryController");
+const blogController = require("./controllers/blogController");
 
 // router
-//app.use("/store", storeController );
+app.use("/store", storeController );
+app.use("/commande", commandeController );
+app.use("/product", productController );
+app.use("/section", sectionController );
+app.use("/category", categoryController );
+app.use("/user", userController );
+app.use("/blog", blogController );
 
-app.listen(port, ()=>{ console.log(`🟢 Server started on port ${port}`); })
+app.listen(port, () => {
+  console.log(`🟢 Server started on port ${port}`);
+});
