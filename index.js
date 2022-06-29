b55e04aeb97058741f5f53cb8af2c1be1f01c787
ahmed <ahmedachour789@gmail.com>
2022-06-29 12:15:24 +0100

    set server on port 3000

---
+++ index.js
@@ -0,0 +1,28 @@
+//immport libs
+const express = require("express");
+const cors = require("cors");
+
+// import database connection
+const mongoose = require("./config/db");
+
+// creation d'un objet express .
+const app = express();
+const port = 3000;
+
+// autorisé les données de type JSON
+app.use(express.json())
+
+// autorisé les données de type files
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// autorisé l'accee d'un serveur
+app.use(cors())
+
+// router
+app.use("/", (req, res)=>{
+    res.status(200).send({message: "hello world"})
+});
+
+app.listen(port, ()=>{ console.log(`🟢 Server started on port ${port}`); })
